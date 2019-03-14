091b46695688b61b00f1f4c5bfac2964339b5145
Mayur Barge <bargemayur05@gmail.com>
2019-03-14 00:22:29 -0700

    Adding logout support and image on home screen

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -7,15 +7,62 @@
 //
 
 import UIKit
+import Parse
+import AlamofireImage
 
-class FeedViewController: UIViewController {
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    
 
+    @IBOutlet weak var tableView: UITableView!
+    var posts = [PFObject]()
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
+    @IBAction func onLogoutButton(_ sender: Any) {
+        PFUser.logOut()
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewController = main.instantiateViewController(withIdentifier: "LoginViewController")
+        
+        let delegate = UIApplication.shared.delegate as! AppDelegate
+        
+        delegate.window?.rootViewController = loginViewController
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        let query = PFQuery(className: "Posts")
+        query.includeKey("author")
+        query.limit = 20
+        
+        query.findObjectsInBackground{ (posts, error) in
+            if posts != nil {
+                self.posts = posts!
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+        let posts = self.posts[indexPath.row]
+        let user = posts["author"] as! PFUser
+        cell.usernameLabel.text = user.username
+        cell.captionLabel.text = posts["caption"] as? String
+        
+        let imageFile = posts["image"] as! PFFileObject
+        let urlString = imageFile.url
+        let url = URL(string: urlString!)
+        cell.photoView.af_setImage(withURL: url!)
+        return cell
+    }
 
     /*
     // MARK: - Navigation
